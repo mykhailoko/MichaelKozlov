c1541c84d82a636e4e094bc44c72d53c7657d1eb
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-09-30 22:45:08 +0300

    Update main.go

--- contest_01/12/main.go
+++ contest_01/12/main.go
@@ -1,1 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x, y int
+	y = 0
+	fmt.Scan(&x)
+	for x != 1 {
+		if x%2 == 0 {
+			x = x / 2
+		} else {
+			x = 3*x + 1
+		}
+		y++
+	}
+	fmt.Print(y)
+}
 
