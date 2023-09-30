f6bb41861656357b106f3e660c8eb9317831506c
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-09-30 22:53:44 +0300

    Update main.cpp

--- contest_01/15/main.cpp
+++ contest_01/15/main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+int main() {
+    std::string input;
+    std::cin >> input;
+
+    input.append ("a");
+
+    char lastChar = input[0];
+    int repeatLength = 1;
+    for (int i = 1; i < input.length(); i++) {
+        if (input[i] == lastChar) {
+            repeatLength++;
+            continue;
+        }
+
+        if (input[i] != lastChar) {
+            std::cout << lastChar;
+            if (repeatLength > 1)
+                std::cout << repeatLength;
+
+            lastChar = input[i];
+            repeatLength = 1;
+        }
+    }
+
+    return 0;
+}
