ed5ebef4778b2e60497a7eb1dc126c37f97f9cdc
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-09-30 22:42:50 +0300

    Update main.go

--- contest_01/10/main.go
+++ contest_01/10/main.go
@@ -1,1 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var swim, wing, neck string
+	fmt.Scanln(&swim, &wing, &neck)
+
+	if swim == "Да" && wing == "Да" && neck == "Да" {
+		fmt.Print("Утка")
+	} else if swim == "Да" && wing == "Да" {
+		fmt.Print("Пингвин")
+	} else if swim == "Да" && neck == "Да" {
+		fmt.Print("Плезиозавры")
+	} else if swim == "Да" {
+		fmt.Print("Дельфин")
+	} else if wing == "Да" && neck == "Да" {
+		fmt.Print("Страус")
+	} else if wing == "Да" {
+		fmt.Print("Курица")
+	} else if neck == "Да" {
+		fmt.Print("Жираф")
+	} else {
+		fmt.Print("Кот")
+	}
+}
 
