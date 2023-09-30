ab2a82b436658f199cd9132d4c02dda3892ab768
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-09-30 22:39:33 +0300

    Update main.cpp

--- contest_01/07/main.cpp
+++ contest_01/07/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+
+    cin >> num1 >> num2;
+
+    if (num1 > num2) {
+        cout << num1 << endl;
+    } else if (num2 > num1) {
+        cout << num2 << endl;
+    } else {
+    	cout << num1 << "=" << num2 << endl;
+    }
+
+}
 
