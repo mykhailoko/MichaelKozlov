961f5a50ed099f86d6aa61778644b0eeb8450bd9
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-09-30 22:57:45 +0300

    Update main.cpp

--- contest_01/19/main.cpp
+++ contest_01/19/main.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+int main() {
+    std::string input;
+    std::set<std::string> words;
+    std::set<std::string> duplicate_words;
+
+    while (true) {
+        std::cin >> input;
+
+        if (input == "end") {
+            break;
+        }
+
+        if (words.count(input) > 0) {
+            duplicate_words.insert(input);
+        } else {
+            words.insert(input);
+        }
+    }
+
+    for (const std::string& word : duplicate_words) {
+        std::cout << word << " ";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
 
