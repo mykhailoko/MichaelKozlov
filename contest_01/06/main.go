ec2751f62aafba0bf36e9ff2d80769584881515b
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-09-30 22:38:37 +0300

    Update main.go

--- contest_01/06/main.go
+++ contest_01/06/main.go
@@ -1,1 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var N int
+	fmt.Scan(&N)
+
+	denominations := []int{5000, 1000, 500, 200, 100}
+	counts := make([]int, len(denominations))
+
+	for i, denom := range denominations {
+		counts[i] = N / denom
+		N %= denom
+	}
+
+	for _, count := range counts {
+		fmt.Print(count, " ")
+	}
+}
 
