c7ed0c09ce0f5b8f4c5ed89edc100d0328fcd285
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-09-30 22:41:35 +0300

    Update main.cpp

--- contest_01/09/main.cpp
+++ contest_01/09/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+#include <math.h>
 
+int main(){
+
+    double a, b;
+    float result; 
+    float resultt;
+
+    cin >> a >> b;
+
+    result = a*a+b*b;
+    resultt = sqrt(result);
+
+    cout << resultt << endl;
+
+}
