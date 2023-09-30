388ffcdf964b245e77f0c4f6815847459959aaa3
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-09-30 22:58:33 +0300

    Update main.cpp

--- contest_01/20/main.cpp
+++ contest_01/20/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    std::string a, b;
+    std::cin >> a >> b;
+
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
+
+    if (a==b) {
+        std::cout << "YES" << std::endl;
+    } else {
+        std::cout << "NO" << std::endl;
+    }
+    return 0;
+}
 
