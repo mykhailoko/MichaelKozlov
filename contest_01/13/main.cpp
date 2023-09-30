da63857ae641de037aa728395c7002c7e1b5566f
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-09-30 22:46:16 +0300

    Update main.cpp

--- contest_01/13/main.cpp
+++ contest_01/13/main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+int main() {
+    int n;
+    std::cin >> n;
+
+    int cur = 0;
+    int len = 1;
+    int max = 2;
+    int ln_step = 1;
+
+    for (int i = 1; i <= n; i++){
+        std::cout << i << ' ';
+
+        cur++;
+        if (cur == len){
+            std::cout << std::endl;
+            len += ln_step;
+            cur = 0;
+        }
+
+        if (len == max || len == 1){
+            ln_step *= -1;
+        }
+        if (len == max) max++;
+    }
+    return 0;
+}
