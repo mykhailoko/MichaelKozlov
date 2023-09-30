85f0adfbafc2f469c74f7d94610d8651b6f318ef
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-09-30 22:55:57 +0300

    Update main.go

--- contest_01/17/main.go
+++ contest_01/17/main.go
@@ -1,1 +1,52 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	heights := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&heights[i])
+	}
+
+	totalWater := 0
+
+	leftMax := make([]int, n)
+	rightMax := make([]int, n)
+
+	leftMax[0] = heights[0]
+	for i := 1; i < n; i++ {
+		leftMax[i] = max(leftMax[i-1], heights[i])
+	}
+
+	rightMax[n-1] = heights[n-1]
+	for i := n - 2; i >= 0; i-- {
+		rightMax[i] = max(rightMax[i+1], heights[i])
+	}
+
+	for i := 0; i < n; i++ {
+		trappedWater := min(leftMax[i], rightMax[i]) - heights[i]
+		if trappedWater > 0 {
+			totalWater += trappedWater
+		}
+	}
+
+	fmt.Println(totalWater)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
