45c0a794d00d81f7985358f9a76e2a4bd832f672
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-09-30 22:56:52 +0300

    Update main.cpp

--- contest_01/18/main.cpp
+++ contest_01/18/main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
+
+std::string getExcelColumnName(int N) {
+    std::string result = "";
+
+    while (N != 0) {
+        N -= 1;
+        char c = (N % 26) + 'A';
+        result = c + result;
+        N /= 26;
+    }
+
+    return result;
+}
+
+int main() {
+    int N;
+    std::cin >> N;
+
+    std::string columnName = getExcelColumnName(N);
+    std::cout << columnName << std::endl;
+
+    return 0;
+}
 
