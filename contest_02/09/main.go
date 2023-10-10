71f1cd7528c2ead2d96608a49450073e026ae6a5
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-10-10 08:47:57 +0300

    Update main.go

--- contest_02/09/main.go
+++ contest_02/09/main.go
@@ -1,1 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func isLucky(ticket string) bool {
+	if len(ticket) != 6 {
+		return false
+	}
+
+	sumFirstHalf := 0
+	sumSecondHalf := 0
+
+	for i := 0; i < 6; i++ {
+		digit := int(ticket[i] - '0')
+
+		if digit < 0 || digit > 9 {
+			return false
+		}
+
+		if i < 3 {
+			sumFirstHalf += digit
+		} else {
+			sumSecondHalf += digit
+		}
+	}
+
+	return sumFirstHalf == sumSecondHalf
+}
+
+func main() {
+	number, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	if isLucky(number[:len(number)-1]) { // Обрезаем \n в конце строки
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
 
