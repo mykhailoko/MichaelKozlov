cb47dd466890dbc3e3f023e1662d3163017ff1d7
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-10-10 08:38:55 +0300

    Update main.cpp

--- contest_02/01/main.cpp
+++ contest_02/01/main.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+bool is_prime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+    if (number == 2) {
+        return true;
+    }
+
+    if (number % 2 == 0) {
+        return false;
+    }
+
+    int sqrt_number = static_cast<int>(std::sqrt(number));
+    for (int i = 3; i <= sqrt_number; i += 2) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int number;
+    std::cin >> number;
+    std::cout << (is_prime(number) ? "YES" : "NO") << std::endl;
+}
 
