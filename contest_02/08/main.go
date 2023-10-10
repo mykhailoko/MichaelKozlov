d3de5a4b0e825c68b72b63e53e084ec587ba698f
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-10-10 08:47:08 +0300

    Update main.go

--- contest_02/08/main.go
+++ contest_02/08/main.go
@@ -1,1 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func isPalindrome(s string) bool {
+	s = toLowerCase(s)
+
+	var cleaned string
+	for _, char := range s {
+		if isAlphaNumeric(char) {
+			cleaned += string(char)
+		}
+	}
+
+	return isPalindromeHelper(cleaned)
+}
+
+func toLowerCase(s string) string {
+	var result string
+	for _, char := range s {
+		if char >= 'A' && char <= 'Z' {
+			result += string(char + 32)
+		} else {
+			result += string(char)
+		}
+	}
+	return result
+}
+
+func isAlphaNumeric(char rune) bool {
+	return (char >= 'a' && char <= 'z') || (char >= '0' && char <= '9')
+}
+
+func isPalindromeHelper(s string) bool {
+	length := len(s)
+	for i := 0; i < length/2; i++ {
+		if s[i] != s[length-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	line, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	if isPalindrome(line[:len(line)-1]) { // Обрезаем \n в конце строки
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
 
