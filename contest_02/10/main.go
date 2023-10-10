d4344f2d70e0c2d0ef180ef46f288e13a06256f0
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-10-10 08:49:15 +0300

    Update main.go

--- contest_02/10/main.go
+++ contest_02/10/main.go
@@ -1,1 +1,39 @@
+package main
+
+import "fmt"
+
+func shift(data []int, steps int) {
+	length := len(data)
+	steps = steps % length
+
+	if steps < 0 {
+		steps = -steps
+		for i := 0; i < steps; i++ {
+			temp := data[0]
+			copy(data[:], data[1:])
+			data[length-1] = temp
+		}
+	} else {
+		for i := 0; i < steps; i++ {
+			temp := data[length-1]
+			copy(data[1:], data[:length-1])
+			data[0] = temp
+		}
+	}
+}
+
+func main() {
+	var steps int
+	fmt.Scan(&steps)
+
+	var data [10]int
+	for index := range data {
+		fmt.Scan(&data[index])
+	}
+
+	shift(data[:], steps)
+	for _, value := range data {
+		fmt.Printf("%d ", value)
+	}
+}
 
