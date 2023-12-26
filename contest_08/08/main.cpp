05475ef2da53610dac88efb3d38499bff697819d
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-12-26 18:41:04 +0200

    Update main.cpp

--- contest_08/08/main.cpp
+++ contest_08/08/main.cpp
@@ -1,1 +1,38 @@
+class ScreenPrintStrategy : public PrintStrategy {
+public:
+    void print(const Document& document) override {
+        std::cout << document.get() << std::endl;
+    }
+};
 
+class StringPrintStrategy : public PrintStrategy {
+    std::string buffer;
+    int docNumber;
+
+public:
+    StringPrintStrategy() : docNumber(1) {}
+
+    void print(const Document& document) override {
+        buffer += "--- doc " + std::to_string(docNumber++) + " ---\n";
+        buffer += document.get() + "\n";
+    }
+
+    std::string getPrintedDocuments() const {
+        return buffer;
+    }
+};
+
+class MockPrintStrategy : public PrintStrategy {
+    int printedDocumentsCount;
+
+public:
+    MockPrintStrategy() : printedDocumentsCount(0) {}
+
+    void print(const Document& document) override {
+        printedDocumentsCount++;
+    }
+
+    int getPrintedDocumentsCount() const {
+        return printedDocumentsCount;
+    }
+};
