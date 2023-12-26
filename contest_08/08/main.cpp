a7d1bcc346e7cb079c7422417843c363796eb186
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-12-26 18:43:00 +0200

    Update main.cpp

--- contest_08/08/main.cpp
+++ contest_08/08/main.cpp
@@ -1,3 +1,46 @@
+#include <iostream>
+#include <string>
+
+class Document{
+    std::string data;
+public:
+    Document(std::string data):data(data){
+    }
+
+    const std::string& get() const{
+        return data;
+    }
+};
+
+class PrintStrategy {
+public:
+    virtual void print(const Document& document) = 0;
+};
+
+
+class Printer {
+    PrintStrategy* strategy;
+    
+public:
+    Printer(PrintStrategy* strategy):strategy(strategy){
+    }
+
+    void setStrategy(PrintStrategy* strategy) {
+        delete this->strategy;
+        this->strategy = strategy;
+    }
+    
+    void print(const Document& doc) {
+        if (strategy == nullptr) exit(1);
+        
+        strategy->print(doc);
+    }
+    
+    ~Printer(){
+        delete strategy;
+    }
+};
+
 class ScreenPrintStrategy : public PrintStrategy {
 public:
     void print(const Document& document) override {
@@ -36,3 +79,36 @@ public:
         return printedDocumentsCount;
     }
 };
+
+int main(){
+    Printer printer(new ScreenPrintStrategy());
+
+    int count;
+    std::cin >> count; std::cin.ignore(1);
+    while(count--){
+        std::string data;
+        std::getline(std::cin, data);
+        printer.print(Document(data)); // Печать документа на экран
+    }
+    
+    StringPrintStrategy* stringStrategy = new StringPrintStrategy();
+    printer.setStrategy(stringStrategy);
+    std::cin >> count; std::cin.ignore(1);
+    while(count--){
+        std::string data;
+        std::getline(std::cin, data);
+        printer.print(Document(data)); // Печать документа в строку
+    }
+    std::cout << stringStrategy->getPrintedDocuments() << std::endl;
+    
+    
+    MockPrintStrategy* mockStrategy = new MockPrintStrategy();
+    printer.setStrategy(mockStrategy);
+    std::cin >> count; std::cin.ignore(1);
+    while(count--){
+        std::string data;
+        std::getline(std::cin, data);
+        printer.print(Document(data)); // Имитация печати, на самом деле просто подсчёт
+    }
+    std::cout << mockStrategy->getPrintedDocumentsCount() << std::endl;
+}
