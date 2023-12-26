04ba1e6b2033859a4632c37ed465184a743d288a
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2023-12-26 18:36:31 +0200

    Update main.cpp

--- contest_08/01/main.cpp
+++ contest_08/01/main.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <cstring>
+
+class Keeper {
+    int trash;
+    int secret;
+
+public:
+    Keeper() {
+        std::cin >> secret;
+    }
+};
+
+struct Hack {
+    int trash;
+    int secret;
+};
+
+int hack_it(Keeper keeper) {
+    Hack h;
+    std::memcpy(&h, &keeper, sizeof(Keeper));
+    return h.secret;
+}
+
+int main() {
+    Keeper keeper;
+    int secret = hack_it(keeper);
+    std::cout << secret;
+}
 
